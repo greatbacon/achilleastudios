254371d454e73ff7faee295dfd4bce375cf8a6f5
greatbacon <4512607+greatbacon@users.noreply.github.com>
2024-07-13 17:07:50 -0600

    Add contact page (#3)

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import insta from '$lib/images/Instagram_simple_icon.svg';
+    const source = "Y291cnRuZXlAYWNoaWxsZWFzdHVkaW9zLmNvbQ==";
+    const target = atob(source);
+</script>
+<h1>Get in Touch</h1>
+<div class="content">
+    <div class="text">
+        <p>
+            📧 {target}
+        </p>
+        <p>
+            <img src={insta} alt="instagram logo"/>
+            <a href="https://instagram.com/achilleastudios">@achilleastudios</a>
+        </p>
+    </div>	
+</div>
+
+<style>
+    .content {        
+        font-size: 2rem;
+        text-align: center;        
+    }
+
+    img {
+        width: 3%;
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+</style>
